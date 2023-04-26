e4c29ed4e3a6226def4a1fa5b1e0482378411fc0
Vidyut Venkatesan <s3925040@student.rmit.edu.au>
2023-04-26 22:58:54 +1000

    Implemented verifyId method, Added some comments

--- Purchase.h
+++ Purchase.h
@@ -1,15 +1,28 @@
 #ifndef PURCHASE_H
 #define PURCHASE_H
 
+#include "LinkedList.h"
+#include <iostream>
+#include <vector>
+
 class Purchase
 {
 private:
+    /**
+     * Checks if user input ID is a valid ID
+     * returns true if InputId is valid, false otherwise
+    */
     bool verifyID(std:: string inputId);
-    
+
+    LinkedList* stocklist;
+
 public:
     Purchase();
     ~Purchase();
 
+    Purchase(LinkedList* stocklist); // Overloaded constructor
+
+    // Shows the purchase menu and accepts user inputs
     void purchaseMenu();
 
 };
