7186cc5ee5fa18d87bbef953b2d3fafa3722721a
Vidyut Venkatesan <s3925040@student.rmit.edu.au>
2023-04-26 22:58:54 +1000

    Implemented verifyId method, Added some comments

--- ppd.cpp
+++ ppd.cpp
@@ -83,13 +83,11 @@ int main(int argc, char **argv)
 
     }
     
-    //REMOVE LATER
-    /*
+    
     std::vector<std::vector<std::string>> stock = LoadFiles::readFile("stock.dat","|");
     LinkedList stockList = LinkedList();
     stockList.addStockToList(stock);
     stockList.printList();
-    */
     
     delete purchase;
     delete stockList;
