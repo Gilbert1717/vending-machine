5299e12f3f25b44cc7b1ad003b7d614ac484b7f2
gilbert <s3665887@student.rmit.edu.au>
2023-04-29 02:06:21 +1000

    remove extra return statement

--- Node.cpp
+++ Node.cpp
@@ -1,5 +1,5 @@
 #include "Node.h"
-
+using std::string;
 Node::Node(){
     
 };
@@ -25,19 +25,19 @@ Stock::Stock(
         unsigned on_hand
 ){
     if (id.length() > IDLEN) {
-        throw std::invalid_argument( "Stock ID is too long" )
+        throw std::invalid_argument( "Stock ID is too long" );
     }
 
     if (name.length() > NAMELEN) {
-        throw std::invalid_argument( "Stock name is too long" )
+        throw std::invalid_argument( "Stock name is too long" );
     }
 
     if (description.length() > DESCLEN) {
-        throw std::invalid_argument( "Stock description is too long" )
+        throw std::invalid_argument( "Stock description is too long" );
     }
     
     if (on_hand < 0) {
-        throw std::invalid_argument( "Stock number cannot be negarive" )
+        throw std::invalid_argument( "Stock number cannot be negarive" );
     }
 
     this->id = id;
@@ -57,16 +57,17 @@ Price::Price(){
 
 Price::Price(unsigned dollars, unsigned cents){
     if ((cents < 0) || (cents > 99)) {
-        throw std::invalid_argument("cents cannot be larger than 99 or negative")
+        throw std::invalid_argument("cents cannot be larger than 99 or negative");
     }
     if ((dollars < 0)) {
-        throw std::invalid_argument("dollars cannot be negative")
+        throw std::invalid_argument("dollars cannot be negative");
     }
     this->dollars = dollars;
     this->cents = cents;
 };
 
 void Price::display(){
+    
     if (this->cents == 0) {
         std::cout << this->dollars << ".00";
     }
