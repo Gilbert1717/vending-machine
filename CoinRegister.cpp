fc4187b7bb1f31c92ae1d1489a333c41ede45ff7
BenNippard <104749340+BenNippard@users.noreply.github.com>
2023-05-01 13:59:55 +1000

    Added Printing Coins using std::cout.width()

--- CoinRegister.cpp
+++ CoinRegister.cpp
@@ -8,23 +8,47 @@ CoinRegister::~CoinRegister(){
 void CoinRegister::display(){
     std::cout << "Coins Summary" << std::endl;
     std::cout << "-------------" << std::endl;
-    std::cout << "Denomination     |   Count" << std::endl;
+    std::cout << "Denomination    |    Count" << std::endl;
     std::cout << "---------------------------" << std::endl;
-    // for (unsigned i = 0; i < NUM_DENOMS; i++){
-    //     Denomination value = this->coins[i].denom;
-    //     int count = this->coins[i].count;
-    //     if (value == FIVE_CENTS){
-    //         std::cout << "5 Cents          | "
-    //     }
-    //     else if (value == TEN_CENTS)
-    //     {
-
-    //     }
+    for (unsigned i = 0; i < NUM_DENOMS; i++){
+        Denomination value = this->coins[i].denom;
+        int count = this->coins[i].count;
+        std::cout.width(16);
+        if (value == FIVE_CENTS){
+            std::cout << std::left << "5 Cents";
+        }
+        else if (value == TEN_CENTS)
+        {
+            std::cout << std::left << "10 Cents";
+        }
+        else if (value == TWENTY_CENTS)
+        {
+            std::cout << std::left << "20 Cents";
+        }
+        else if (value == FIFTY_CENTS)
+        {
+            std::cout << std::left << "50 Cents";
+        }
+        else if (value == ONE_DOLLAR)
+        {
+            std::cout << std::left << "1 Dollar";
+        }
+        else if (value == TWO_DOLLARS)
+        {
+            std::cout << std::left << "2 Dollar";
+        }
+        else if (value == FIVE_DOLLARS)
+        {
+            std::cout << std::left << "5 Dollar";
+        }
+        else if (value == TEN_DOLLARS)
+        {
+            std::cout << std::left << "10 Dollar";
+        }
+        std::cout << "|";
+        std::cout.width(9);
+        std::cout << std::right << count;
         
-    // }
-    for(unsigned int i = 0; i < NUM_DENOMS; i++){
-        std::cout << "denom:" << (this->coins[i].denom);
-        std::cout << "|Count:"<<  (this->coins[i].count) << std::endl;
     }
 }
 void CoinRegister::change(){
